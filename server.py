aad622a6c75f492de451a36bc78362374fa05d8e
Gamidu Welikanna <gamidukaveen@gmail.com>
2026-05-06 15:29:26 +0530

    fix: city not found edge case code is fixed

--- server.py
+++ server.py
@@ -1,18 +1,31 @@
 from flask import Flask, render_template, request
-from weather import get_current_weather 
+from weather import get_current_weather
 from waitress import serve
 
 app = Flask(__name__)
+
+
 @app.route('/')
 @app.route('/index')
-
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
 
 @app.route('/weather')
-def weather():
+def get_weather():
     city = request.args.get('city')
+
+    # Check for empty strings or string with only spaces
+    if not bool(city.strip()):
+        # You could render "City Not Found" instead like we do below
+        city = "Kansas City"
+
     weather_data = get_current_weather(city)
+
+    # City is not found by API
+    if not weather_data['cod'] == 200:
+        return render_template('city-not-found.html')
+
     return render_template(
         "weather.html",
         title=weather_data["name"],
@@ -21,5 +34,6 @@ def weather():
         feels_like=f"{weather_data['main']['feels_like']:.1f}"
     )
 
-if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8000)
+
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=8000)
