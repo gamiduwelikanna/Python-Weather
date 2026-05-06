ff0e80874a4fbaa5b346ede12872c1927fd56dab
Gamidu Welikanna <gamidukaveen@gmail.com>
2026-05-06 14:41:15 +0530

    fix: added dependecies and fixed some issues

--- server.py
+++ server.py
@@ -1,2 +1,13 @@
-from Flask import Flask, rendor_template, request
+from flask import Flask, render_template, request
 from weather import get_current_weather 
+from waitress import serve
+
+app = Flask(__name__)
+@app.route('/')
+@app.route('/index')
+
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    serve(app, host='0.0.0.0', port=8000)
